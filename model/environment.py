8664893b25a06667a5e8232a935b538e92c8a7f9
Mauriyin <haoyin@uw.edu>
2020-09-17 16:56:38 -0700

    reformat

--- model/environment.py
+++ model/environment.py
@@ -1,47 +1,51 @@
-import numpy as np 
+import numpy as np
 import random
 
+
 class ENVIRONMENT(object):
-	"""docstring for ENVIRONMENT"""
-	def __init__(self,
-				 n_actions,
-				 rb_leader, 
-				 rb_member,
-				 rb_hidden,
-				 state_size = 32,
-				 window_size = 1,
-				 ):
-		super(ENVIRONMENT, self).__init__()
-		self.state_size = state_size
-		self.window_size = window_size
-		self.rb_leader = rb_leader
-		self.rb_member = rb_member
-		self.rb_hidden = rb_hidden
-		self.n_actions = n_actions
-		self.n_nodes = 2
-		
-	def reset(self):
-		state = []
-		for i in range(int(self.state_size/2)):
-			action_index = np.where(self.rb_leader[i] == 0)
-			action_size_period = len(action_index[0])
-			action_sel = np.random.choice(action_size_period)
-			action = action_index[0][action_sel]           #The leader vehicle choose an idle RB to send
-			obs = int(self.rb_hidden[i][action] > 0 )        #The leader vehicle will receive an ack/nack from the last member vehicle
-			state.append(action)
-			state.append(obs)
-		state = np.array(state)
-		return state
+    """docstring for ENVIRONMENT"""
+    def __init__(
+        self,
+        n_actions,
+        rb_leader,
+        rb_member,
+        rb_hidden,
+        state_size=32,
+        window_size=1,
+    ):
+        super(ENVIRONMENT, self).__init__()
+        self.state_size = state_size
+        self.window_size = window_size
+        self.rb_leader = rb_leader
+        self.rb_member = rb_member
+        self.rb_hidden = rb_hidden
+        self.n_actions = n_actions
+        self.n_nodes = 2
 
-	def step(self, action, time):
-		reward = 0
-		observation_ = int(self.rb_hidden[time][action] + self.rb_leader[time+1][action] > 0 )
-		reward = int(observation_ == 0 )
+    def reset(self):
+        state = []
+        for i in range(int(self.state_size / 2)):
+            action_index = np.where(self.rb_leader[i] == 0)
+            action_size_period = len(action_index[0])
+            action_sel = np.random.choice(action_size_period)
+            #The leader vehicle choose an idle RB to send
+            action = action_index[0][action_sel]
+            #The leader vehicle will receive an ack/nack from the last member
+            obs = int(self.rb_hidden[i][action] > 0)
+            state.append(action)
+            state.append(obs)
+        state = np.array(state)
+        return state
 
-		return observation_, reward
+    def step(self, action, time):
+        reward = 0
+        observation_ = int(self.rb_hidden[time][action] +
+                           self.rb_leader[time + 1][action] > 0)
+        reward = int(observation_ == 0)
 
+        return observation_, reward
 
-	def test_update(self, rb_leader, rb_member):
-		self.rb_leader = rb_leader
-		self.rb_member = rb_member
-		self.rb_hidden = rb_member
+    def test_update(self, rb_leader, rb_member):
+        self.rb_leader = rb_leader
+        self.rb_member = rb_member
+        self.rb_hidden = rb_member
--- utils/analysis.py
+++ utils/analysis.py
@@ -1,20 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd 
+import pandas as pd
 
 p = [0.1, 0.3, 0.5]
-col_prob_analy = np.zeros([3,20])
-n = np.arange(10,210,10)
-Ts= 10
+col_prob_analy = np.zeros([3, 20])
+n = np.arange(10, 210, 10)
+Ts = 10
 
 for k in range(3):
     i = 0
     for density in range(10, 210, 10):
         Nv = density * 0.8
         N_ht = density * 0.1
-        P_single = (200 - density * 0.4 - 1)/(200 - density * 0.4)
-        pc = 1 - (1-p[k]/(Ts*200))**(Nv -1)
-        col_prob_analy[k][i] = 1-(1-pc)*P_single**N_ht
+        P_single = (200 - density * 0.4 - 1) / (200 - density * 0.4)
+        pc = 1 - (1 - p[k] / (Ts * 200))**(Nv - 1)
+        col_prob_analy[k][i] = 1 - (1 - pc) * P_single**N_ht
         i = i + 1
 
 plt.plot(n, col_prob_analy[0], '-*')
